85c786919bd49322d2a040eeb2e74bd8f58596bf
moquem <maena.quemener@yahoo.com>
2022-12-01 11:11:07 +0100

    Generation de tests sat et unsat

--- src/generator_horn.py
+++ src/generator_horn.py
@@ -2,8 +2,8 @@ import random
 import sys
 import os
 
-MAX_VAR = 300
-MAX_CLAUSE = 50000
+MAX_VAR = 10000
+MAX_CLAUSE = 500000
 
 def sat_random_horn(n):
     model = [random.randint(0,1) for _ in range(n)]        # On génère un modèle et on va construire des clauses qui le respecte.
@@ -26,6 +26,82 @@ def sat_random_horn(n):
 
     return hs
 
+def unsat_random_horn(n):  # n > 0
+    nb_pos = 0
+    nb_neg = 0
+    pos = set()
+    neg = set()
+    not_assigned = set(range(1,n+1))
+
+    hs = set()
+
+    l_sgl = random.randint(1,n)          # On construit les singletons qui imposent des valeurs.
+    for _ in range(l_sgl):
+        pos_neg = random.randint(0,1)
+        v = random.sample(not_assigned,1)
+        not_assigned.remove(v[0])
+        if pos_neg:
+            pos.add(v[0])
+            nb_pos += 1
+        else:
+            neg.add(v[0])
+            v[0] = -v[0]
+            nb_neg += 1
+        hs.add(frozenset(v))
+
+    nb_clause = random.randint(0,int(MAX_CLAUSE/(MAX_VAR+1-n)))
+    for _ in range(nb_clause):                                        # On construit des clauses de manière à ce que toutes les variables soient déjà assignées sauf une -> impose son assignation.
+        if nb_pos + nb_neg < n:
+            sample_size = random.randint(0,nb_pos)
+            cl = random.sample(pos,sample_size)
+            for i in range(len(cl)):
+                cl[i] = -cl[i]
+            value = random.randint(0,min(1,nb_neg))
+            if value:
+                v = random.sample(neg,1)
+                cl.append(v[0])
+                v = random.sample(not_assigned,1)
+                cl.append(-v[0])
+                not_assigned.remove(v[0])
+                neg.add(v[0])
+                nb_neg += 1
+            else:
+                value = random.randint(0,1)
+                v = random.sample(not_assigned,1)
+                not_assigned.remove(v[0])
+                if value:
+                    pos.add(v[0])
+                    nb_pos += 1
+                    cl.append(v[0])
+                else:
+                    neg.add(v[0])
+                    nb_neg += 1
+                    cl.append(-v[0])
+            hs.add(frozenset(cl))
+    
+    v = None                          # Ajout d'une clause unsat.
+    if nb_neg == 0:
+        deb = 1
+                
+    else:
+        deb = 0
+        v = random.sample(neg,1)
+
+    sample_size = random.randint(deb,nb_pos)
+    cl = random.sample(pos,sample_size)
+    for i in range(len(cl)):
+        cl[i] = -cl[i]
+    
+    if v != None:
+        cl.append(v[0])
+
+    hs.add(frozenset(cl))
+    return hs
+
+            
+
+    
+
 def sat_format_horn(nb_var,hs_model,id):     # Permet de créer des fichiers tests.cnf
     outputFile_hs = open(os.path.join('/home/maena/Documents/Etude/ENS Paris-Saclay/M1/logia/part2/HornSAT/tests/HS/OK/test'+str(nb_var)+'_'+str(id)+'.cnf'),"w")
     outputFile_cnf = open(os.path.join('/home/maena/Documents/Etude/ENS Paris-Saclay/M1/logia/part2/HornSAT/tests/DPLL/OK/test'+str(nb_var)+'_'+str(id)+'.cnf'),"w")
@@ -42,10 +118,38 @@ def sat_format_horn(nb_var,hs_model,id):     # Permet de créer des fichiers tes
         outputFile_hs.writelines(cl+"\n")
         outputFile_cnf.writelines(cl+"\n")
 
+def unsat_format_horn(nb_var,hs_model,id):     # Permet de créer des fichiers tests.cnf
+    outputFile_hs = open(os.path.join('/home/maena/Documents/Etude/ENS Paris-Saclay/M1/logia/part2/HornSAT/tests/HS/KO/test'+str(nb_var)+'_'+str(id)+'.cnf'),"w")
+    outputFile_cnf = open(os.path.join('/home/maena/Documents/Etude/ENS Paris-Saclay/M1/logia/part2/HornSAT/tests/DPLL/KO/test'+str(nb_var)+'_'+str(id)+'.cnf'),"w")
+   
 
+    outputFile_hs.writelines("p hs "+str(nb_var)+" "+str(len(hs_model))+"\n")
+    outputFile_cnf.writelines("p cnf "+str(nb_var)+" "+str(len(hs_model))+"\n")
+
+    for i in hs_model:
+        cl = ""
+        for elt in i:
+            cl += str(elt)+" "
+        cl += "0"
+        outputFile_hs.writelines(cl+"\n")
+        outputFile_cnf.writelines(cl+"\n")
+
+"""
 for i in range(1,MAX_VAR+1):
     test_1 = random.randint(0,1)   # On ne fait pas des tests forcément pour chaque nombre de variables.
     test_2 = random.randint(0,1)
-    nb_test = random.randint(1,3)       # Combien de tests on fait pour ce nombre.
-    for id_test in range(test_1*test_2*nb_test):
+    test_3 = random.randint(0,1)
+    test_4 = random.randint(0,1)
+    nb_test = random.randint(0,3)       # Combien de tests on fait pour ce nombre.
+    for id_test in range(test_1*test_2*test_3*test_4*nb_test):
         sat_format_horn(i,sat_random_horn(i),id_test)
+"""
+
+for i in range(1,100):
+    test_1 = random.randint(0,1)   # On ne fait pas des tests forcément pour chaque nombre de variables.
+    test_2 = random.randint(0,1)
+    test_3 = random.randint(0,1)
+    test_4 = random.randint(0,1)
+    nb_test = random.randint(0,3)       # Combien de tests on fait pour ce nombre.
+    for id_test in range(test_1*test_2*test_3*test_4*nb_test):
+        unsat_format_horn(i,unsat_random_horn(i),id_test)
--- tests/run.sh
+++ tests/run.sh
@@ -1,44 +1,74 @@
 #!/bin/bash
 
-pass=0
-wrong=0
-timeout=0
-fail=0
+pass_dpll=0
+pass_hs=0
+pass_xnf=0
 
+wrong_dpll=0
+wrong_hs=0
+wrong_xnf=0
 
-OKFILES=$(find ./tests/OK -name "*.cnf")
-KOFILES=$(find ./tests/KO -name "*.cnf")
+timeout_dpll=0
+timeout_hs=0
+timeout_xnf=0
+
+fail_dpll=0
+fail_hs=0
+fail_xnf=0
+
+OKDPLLFILES=$(find ./tests/DPLL/OK -name "*.cnf") 
+KODPLLFILES=$(find ./tests/DPLL/KO -name "*.cnf")
+OKHSFILES=$(find ./tests/HS/OK -name "*.cnf")
+KOHSFILES=$(find ./tests/HS/KO -name "*.cnf")
+OKXNFFILES=$(find ./tests/XNF/OK -name "*.cnf")
+KOXNFFILES=$(find ./tests/XNF/KO -name "*.cnf")
+
+# ?
 echo "find ./tests/KO -path '$KODIR/*.cnf'"
-TOTALOK=$(wc -w <<< "$OKFILES")
-TOTALKO=$(wc -w <<< "$KOFILES")
-TOTAL=$((TOTALOK+TOTALKO))
-TIMEOUT=$1
+
+TOTALOKDPLL=$(wc -w <<< "$OKDPLLFILES")
+TOTALKODPLL=$(wc -w <<< "$KODPLLFILES")
+TOTALOKHS=$(wc -w <<< "$OKHSFILES")
+TOTALKOHS=$(wc -w <<< "$KOHSFILES")
+TOTALOKXNF=$(wc -w <<< "$OKXNFFILES")
+TOTALKOXNF=$(wc -w <<< "$KOXNFFILES")
+
+TOTALDPLL=$((TOTALOKDPLL+TOTALKODPLL))
+TOTALHS=$((TOTALOKHS+TOTALKOHS  ))
+TOTALXNF=$((TOTALOKXNF+TOTALKOXNF ))
+
+# TOTAL=$((TOTALDPLL+TOTALHS+TOTALXNF))
+TIMEOUT=30
 CMD="timeout $TIMEOUT ./dpll"
 
 
-echo ""
+### DPLL 
+
+echo " "
 echo "------------------------"
-echo "  Running $TOTALOK satisfiable tests with TIMEOUT=$TIMEOUT"
+echo "  DPLL - $TOTALDPLL TESTS"
+echo "------------------------"
+echo "  Running $TOTALOKDPLL satisfiable tests with TIMEOUT=$TIMEOUT"
 echo "------------------------"
 
-for i in $OKFILES ; do
+for i in $OKDPLLFILES ; do
     echo -n "$i... " ;
     ret=$($CMD $i);
     if [ $? -eq 124 ];
     then
-	timeout=$((timeout+1));
+	timeout_dpll=$((timeout_dpll+1));
 	echo -e "\033[0;33mTIMEOUT\033[0m";
     else
 	if echo $ret | grep "true" > /dev/null;
 	then
-	    pass=$((pass+1));
+	    pass_dpll=$((pass_dpll+1));
 	    echo -e "\033[0;32mSUCCESS\033[0m"
 	elif echo $ret | grep "false" > /dev/null;
 	then
-	    wrong=$((wrong+1));
+	    wrong_dpll=$((wrong_dpll+1));
 	    echo -e "\033[0;31mWRONG\033[0m";
 	else
-	    fail=$((fail+1));
+	    fail_dpll=$((fail_dpll+1));
 	    echo -e "\033[0;36mFAIL\033[0m";
 	fi;
     fi;
@@ -46,35 +76,170 @@ done;
 
 echo ""
 echo "------------------------"
-echo "  Running $TOTALKO unsatisfiable tests with TIMEOUT=$TIMEOUT"
+echo "  Running $TOTALKODPLL unsatisfiable tests with TIMEOUT=$TIMEOUT"
 echo "------------------------"
 
-for i in $KOFILES ; do
+for i in $KODPLLFILES ; do
     echo -n "$i... " ;
     ret=$($CMD $i);
     if [ $? -eq 124 ];
     then
-	timeout=$((timeout+1));
+	timeout_dpll=$((timeout_dpll+1));
 	echo -e "\033[0;33mTIMEOUT\033[0m";
     else
 	if echo $ret | grep "true" > /dev/null;
 	then
-	    wrong=$((wrong+1));
+	    wrong_dpll=$((wrong_dpll+1));
 	    echo -e "\033[0;31mWRONG\033[0m";
 	elif echo $ret | grep "false" > /dev/null;
 	then
-	    pass=$((pass+1));
+	    pass_dpll=$((pass_dpll+1));
 	    echo -e "\033[0;32mSUCCESS\033[0m"
 	else
-	    fail=$((fail+1));
+	    fail_dpll=$((fail_dpll+1));
 	    echo -e "\033[0;36mFAIL\033[0m";
 	fi;
     fi;
 done;
 
 echo "------------------------"
-echo -e "\033[0;31mWrong  : $wrong / $TOTAL\033[0m"
-echo -e "\033[0;32mPass   : $pass / $TOTAL\033[0m"
-echo -e "\033[0;33mTimeout: $timeout / $TOTAL\033[0m"
-echo -e "\033[0;36mFail   : $fail / $TOTAL\033[0m"
+echo -e "\033[0;31mWrong  : $wrong_dpll / $TOTALDPLL\033[0m"
+echo -e "\033[0;32mPass   : $pass_dpll / $TOTALDPLL\033[0m"
+echo -e "\033[0;33mTimeout: $timeout_dpll / $TOTALDPLL\033[0m"
+echo -e "\033[0;36mFail   : $fail_dpll / $TOTALDPLL\033[0m"
+
+
+### HS
+
+echo " "
+echo "------------------------"
+echo "  HORNSAT - $TOTALHS TESTS"
+echo "------------------------"
+echo "  Running $TOTALOKHS satisfiable tests with TIMEOUT=$TIMEOUT"
+echo "------------------------"
+
+for i in $OKHSFILES ; do
+    echo -n "$i... " ;
+    ret=$($CMD $i);
+    if [ $? -eq 124 ];
+    then
+	timeout_hs=$((timeout_hs+1));
+	echo -e "\033[0;33mTIMEOUT\033[0m";
+    else
+	if echo $ret | grep "true" > /dev/null;
+	then
+	    pass_hs=$((pass_hs+1));
+	    echo -e "\033[0;32mSUCCESS\033[0m"
+	elif echo $ret | grep "false" > /dev/null;
+	then
+	    wrong_hs=$((wrong_hs+1));
+	    echo -e "\033[0;31mWRONG\033[0m";
+	else
+	    fail_hs=$((fail_hs+1));
+	    echo -e "\033[0;36mFAIL\033[0m";
+	fi;
+    fi;
+done;
+
+echo ""
+echo "------------------------"
+echo "  Running $TOTALKOHS unsatisfiable tests with TIMEOUT=$TIMEOUT"
+echo "------------------------"
+
+for i in $KOHSFILES ; do
+    echo -n "$i... " ;
+    ret=$($CMD $i);
+    if [ $? -eq 124 ];
+    then
+	timeout_hs=$((timeout_hs+1));
+	echo -e "\033[0;33mTIMEOUT\033[0m";
+    else
+	if echo $ret | grep "true" > /dev/null;
+	then
+	    wrong_hs=$((wrong_hs+1));
+	    echo -e "\033[0;31mWRONG\033[0m";
+	elif echo $ret | grep "false" > /dev/null;
+	then
+	    pass_hs=$((pass_hs+1));
+	    echo -e "\033[0;32mSUCCESS\033[0m"
+	else
+	    fail_hs=$((fail_hs+1));
+	    echo -e "\033[0;36mFAIL\033[0m";
+	fi;
+    fi;
+done;
+
+echo "------------------------"
+echo -e "\033[0;31mWrong  : $wrong_hs / $TOTALHS\033[0m"
+echo -e "\033[0;32mPass   : $pass_hs / $TOTALHS\033[0m"
+echo -e "\033[0;33mTimeout: $timeout_hs / $TOTALHS\033[0m"
+echo -e "\033[0;36mFail   : $fail_hs / $TOTALHS\033[0m"
+
+
+
+### XNF
+
+echo " "
+echo "------------------------"
+echo "  XNF - $TOTALXNF TESTS"
+echo "------------------------"
+echo "  Running $TOTALOKXNF satisfiable tests with TIMEOUT=$TIMEOUT"
+echo "------------------------"
+
+for i in $OKXNFFILES ; do
+    echo -n "$i... " ;
+    ret=$($CMD $i);
+    if [ $? -eq 124 ];
+    then
+	timeout_xnf=$((timeout_xnf+1));
+	echo -e "\033[0;33mTIMEOUT\033[0m";
+    else
+	if echo $ret | grep "true" > /dev/null;
+	then
+	    pass_xnf=$((pass_xnf+1));
+	    echo -e "\033[0;32mSUCCESS\033[0m"
+	elif echo $ret | grep "false" > /dev/null;
+	then
+	    wrong_xnf=$((wrong_xnf+1));
+	    echo -e "\033[0;31mWRONG\033[0m";
+	else
+	    fail_xnf=$((fail_xnf+1));
+	    echo -e "\033[0;36mFAIL\033[0m";
+	fi;
+    fi;
+done;
+
+echo ""
+echo "------------------------"
+echo "  Running $TOTALKOXNF unsatisfiable tests with TIMEOUT=$TIMEOUT"
+echo "------------------------"
+
+for i in $KOXNFFILES ; do
+    echo -n "$i... " ;
+    ret=$($CMD $i);
+    if [ $? -eq 124 ];
+    then
+	timeout_xnf=$((timeout_xnf+1));
+	echo -e "\033[0;33mTIMEOUT\033[0m";
+    else
+	if echo $ret | grep "true" > /dev/null;
+	then
+	    wrong_xnf=$((wrong_xnf+1));
+	    echo -e "\033[0;31mWRONG\033[0m";
+	elif echo $ret | grep "false" > /dev/null;
+	then
+	    pass_xnf=$((pass_xnf+1));
+	    echo -e "\033[0;32mSUCCESS\033[0m"
+	else
+	    fail_xnf=$((fail_xnf+1));
+	    echo -e "\033[0;36mFAIL\033[0m";
+	fi;
+    fi;
+done;
+
+echo "------------------------"
+echo -e "\033[0;31mWrong  : $wrong_xnf / $TOTALXNF\033[0m"
+echo -e "\033[0;32mPass   : $pass_xnf / $TOTALXNF\033[0m"
+echo -e "\033[0;33mTimeout: $timeout_xnf / $TOTALXNF\033[0m"
+echo -e "\033[0;36mFail   : $fail_xnf / $TOTALXNF\033[0m"
 exit 0
